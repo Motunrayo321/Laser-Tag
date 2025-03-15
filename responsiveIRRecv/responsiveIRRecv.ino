1c035cdaf2c55660cab84a636e2e6938441f981a
unknown <samlizzy353@gmail.com>
2025-03-15 22:35:33 +0100

    Included ID for trans and receive

--- responsiveIRRecv/responsiveIRRecv.ino
+++ responsiveIRRecv/responsiveIRRecv.ino
@@ -1,8 +1,9 @@
 #include <IRremote.h>
 
-const int RECV_PIN = 14;  // Pin connected to the IR receiver
-int LED = 27;
-ind ID = 1;
+const int RECV_PIN = 25;  // Pin connected to the IR receiver
+int LED = 33;
+int playerID = 1;
+int shooterID;
 
 void setup() {
   Serial.begin(115200);
@@ -22,16 +23,49 @@ void loop() {
     uint16_t command = IrReceiver.decodedIRData.command;
 
     // Print the command code
-    Serial.print("Command: 0x");
-    Serial.println(command);
+    //Serial.print("Command: 0x");
+    //Serial.println(command);
 
     // Resume receiving the next signal
     IrReceiver.resume();
 
     if (command == 22) {
+      Serial.println("Warning Shot!");
+    }
+
+    if (command == 12 || command == 24 || command == 94 || command == 8 || command == 28) {
       digitalWrite(LED, LOW);
-      delay(500);
+      delay(100);
       digitalWrite(LED, HIGH);
+
+      Serial.print("You were shot by Player ");
+      Serial.println(ShooterID(command));
     }
   }
+}
+
+
+int ShooterID(int code) {
+  switch (code) {
+    case 12:
+    shooterID = 1;
+    break;
+
+    case 24:
+    shooterID = 2;
+    break;
+
+    case 94:
+    shooterID = 3;
+    break;
+
+    case 8:
+    shooterID = 4;
+    break;
+
+    case 28:
+    shooterID = 5;
+    break; 
+  }
+  return shooterID;
 }
